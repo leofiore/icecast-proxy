6752c2e2351ab97876b0edaec9da84328ced8065
leonardo <leofiore@libero.it>
2014-09-12 17:28:12 +0200

    Memory Transcoding Threading

--- memory.py
+++ memory.py
@@ -5,20 +5,13 @@ from select import select
 import logging
 import fcntl
 import os
+from Queue import Queue
 
 
 logger = logging.getLogger('server.memory')
 
+
 class cStringTranscoder:
-
-    decode_flac = 'flac --totally-silent -s -d --force-raw-format ' \
-        '--sign=signed --endian=little -o - -'
-
-    decode_mpeg = 'madplay -q -b 16 -R 44100 -S -o raw:- -'
-    encode_mpeg = 'lame --quiet --preset cbr {bitrate} -r -s 44.1 --bitwidth 16 - -'
-
-    decode_ogg = 'oggdec -Q -R -b 16 -e 0 -s 1 -o - -'
-    encode_ogg = 'oggenc -Q --ignorelength -r -b {bitrate} -B 16 -C 2 -R 44100 --raw-endianness 0 -q 1.5 -'
 
     def __init__(self, infmt, outfmt):
         self.buffer = StringIO()
@@ -32,23 +25,9 @@ class cStringTranscoder:
         self.not_full = threading.Condition(self.mutex)
         self.end = False
         if infmt == outfmt:
-            self.decproc = None
-            self.encproc = None
+            self.transcoder = TranscoderThread(infmt, outfmt)
         else:
-            logger.info("Buffer will activate transcoding")
-            dec = getattr(self, 'decode_' + infmt[0])
-            enc = getattr(self, 'encode_' + outfmt[0]).format(bitrate=outfmt[1])
-            self.decproc = Popen(
-                dec.split(),
-                stdin=PIPE, stdout=PIPE
-            )
-            self.encproc = Popen(
-                enc.split(),
-                stdin=self.decproc.stdout, stdout=PIPE
-            )
-            fd = self.encproc.stdout.fileno()
-            fl = fcntl.fcntl(fd, fcntl.F_GETFL)
-            fcntl.fcntl(fd, fcntl.F_SETFL, fl | os.O_NONBLOCK)
+            self.transcoder = None
 
     def write(self, data_in):
         if self.end:
@@ -60,31 +39,8 @@ class cStringTranscoder:
                 retries = retries + 1
                 if retries > 2:
                     raise Exception()
-            if self.decproc and self.encproc:
-                data_sent = False
-                data = None
-                try:
-                    while not self.end and not data_sent and not data:
-                        #logger.debug("Processing encode/decode")
-                        rlist, wlist, xlist = select(
-                            [self.encproc.stdout, self.decproc.stdout],
-                            [self.decproc.stdin],
-                            [],
-                            5
-                        )
-                        if len(wlist) and not data_sent:
-                            self.decproc.stdin.write(data_in)
-                            data_sent = True
-                            #logger.debug("wrote to decoder")
-                        if len(rlist) == 2:
-                            data = self.encproc.stdout.read(8192)
-                            if not len(data):
-                                return
-                            #logger.debug("read from encoder %s", len(data))
-                        elif not len(rlist) and data_sent:
-                            return
-                except IOError as err:
-                    logger.error(err)
+            if self.transcoder:
+                data = self.transcoder.read()
             else:
                 data = data_in
             if not data:
@@ -126,6 +82,90 @@ class cStringTranscoder:
 
     def close(self):
         self.end = True
+        if self.transcoder:
+            self.transcoder.close()
+        try:
+            self.buffer.close()
+            del self.buffer
+        except Exception as e:
+            logger.error(e)
+        logger.debug("client closed")
+
+
+class TranscoderThread(threading.Thread):
+
+    decode_flac = 'flac --totally-silent -s -d --force-raw-format ' \
+        '--sign=signed --endian=little -o - -'
+
+    decode_mpeg = 'madplay -q -b 16 -R 44100 -S -o raw:- -'
+    encode_mpeg = 'lame --quiet --preset cbr {bitrate} -r -s 44.1 --bitwidth 16 - -'
+
+    decode_ogg = 'oggdec -Q -R -b 16 -e 0 -s 1 -o - -'
+    encode_ogg = 'oggenc -Q --ignorelength -r -b {bitrate} -B 16 -C 2 -R 44100 --raw-endianness 0 -q 1.5 -'
+
+    def __init__(self, infmt, outfmt):
+
+        super(TranscoderThread, self).__init__()
+
+        self.end = False
+        if infmt == outfmt:
+            self.decproc = None
+            self.encproc = None
+        else:
+            logger.info("Buffer will activate transcoding")
+            dec = getattr(self, 'decode_' + infmt[0])
+            enc = getattr(self, 'encode_' + outfmt[0]).format(bitrate=outfmt[1])
+            self.decproc = Popen(
+                dec.split(),
+                stdin=PIPE, stdout=PIPE
+            )
+            self.encproc = Popen(
+                enc.split(),
+                stdin=self.decproc.stdout, stdout=PIPE
+            )
+            fd = self.encproc.stdout.fileno()
+            fl = fcntl.fcntl(fd, fcntl.F_GETFL)
+            fcntl.fcntl(fd, fcntl.F_SETFL, fl | os.O_NONBLOCK)
+            self.readqueue = Queue()
+            self.writequeue = Queue()
+
+    def run(self):
+        while not self.end:
+            if self.decproc and self.encproc:
+                data_sent = False
+                data = None
+                data_in = self.writequeue.get()
+                try:
+                    while not self.end and not data_sent and not data:
+                        #logger.debug("Processing encode/decode")
+                        rlist, wlist, xlist = select(
+                            [self.encproc.stdout, self.decproc.stdout],
+                            [self.decproc.stdin],
+                            [],
+                            5
+                        )
+                        if len(wlist) and not data_sent:
+                            self.decproc.stdin.write(data_in)
+                            data_sent = True
+                            #logger.debug("wrote to decoder")
+                        if len(rlist) == 2:
+                            data = self.encproc.stdout.read(8192)
+                            if len(data):
+                                self.readqueue.put(data)
+                            #logger.debug("read from encoder %s", len(data))
+                        #elif not len(rlist) and data_sent:
+                        #    return
+                except IOError as err:
+                    logger.error(err)
+
+    def read(self):
+        return self.readqueue.get()
+
+    def write(self, data):
+        self.writequeue.put(data)
+
+    def close(self):
+        self.end = True
         try:
             if self.encproc:
                 self.encproc.stdout.close()
@@ -134,11 +174,5 @@ class cStringTranscoder:
                 self.decproc.stdin.close()
                 self.decproc.stdout.close()
                 self.decproc.kill()
-        except Exception as e:
-            logger.error(e)
-        try:
-            self.buffer.close()
-            del self.buffer
-        except Exception as e:
-            logger.error(e)
-        logger.debug("client closed")
+        except Exception as err:
+            logger.exception("Error closing transcoder")
